21a8f42f4f96dbe15910826edb740c614d7bdc2d
AdamTV <43098728+AdamTV@users.noreply.github.com>
2020-11-10 22:34:34 -0500

    instanceof check on PostComponent

--- web422-a4/src/app/app.component.ts
+++ web422-a4/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PostComponent } from './post/post.component';
 
 @Component({
   selector: 'app-root',
@@ -7,14 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'web422-a4';
-  onActivate(event) {
-        let scrollToTop = window.setInterval(() => {
-            let pos = window.pageYOffset;
-            if (pos > 0) {
-                window.scrollTo(0, pos - 20); // how far to scroll on each step
-            } else {
-                window.clearInterval(scrollToTop);
-            }
-        }, 16);
+    onActivate(event) {
+        console.log(event);
+        if (event instanceof PostComponent) {
+            let scrollToTop = window.setInterval(() => {          
+                let pos = window.pageYOffset;
+                if (pos > 0) {
+                   window.scrollTo(0, pos - 50); // how far to scroll on each step
+                } else {
+                    window.clearInterval(scrollToTop);
+                }
+            }, 16);
+        }
     }
 }
--- web422-a4/src/app/post/post.component.ts
+++ web422-a4/src/app/post/post.component.ts
@@ -16,7 +16,15 @@ export class PostComponent implements OnInit {
 
   constructor(private route: ActivatedRoute) {
     this.route.params.subscribe(params => {
-      this.id = params.id;      
+      this.id = params.id;    
+      let scrollToTop = window.setInterval(() => {
+            let pos = window.pageYOffset;
+            if (pos > 0) {
+                window.scrollTo(0, pos - 50); // how far to scroll on each step
+            } else {
+                window.clearInterval(scrollToTop);
+            }
+        }, 16);
     });
    }
 
