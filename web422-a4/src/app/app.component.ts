ad35872b5e70b841020cae8823958ef2358fa180
AdamTV <43098728+AdamTV@users.noreply.github.com>
2020-11-11 16:40:30 -0500

    ASSIGNMENT COMMENT DECL

--- web422-a4/src/app/app.component.ts
+++ web422-a4/src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*********************************************************************************
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: ADAM STINZIANI      Student ID: 124521188       Date: 2020-11-11
+*
+********************************************************************************/
+
 import { Component } from '@angular/core';
 import { PostComponent } from './post/post.component';
 
