fa8283a0fbabd34139c5991691fcbcd6933dbd1f
AdamTV <43098728+AdamTV@users.noreply.github.com>
2020-11-23 11:20:56 -0500

    init

---
+++ wk8(end)-demo(ng)-app/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AttributeClassStyleBindingsComponent } from './attribute-class-style-bindings/attribute-class-style-bindings.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { EventsComponent } from './events/events.component';
+import { ChildComponent } from './child/child.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+// import HttpClientModule from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { PostsComponent } from './posts/posts.component';
+import { PostComponent } from './post/post.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AttributeClassStyleBindingsComponent,
+    DirectivesComponent,
+    EventsComponent,
+    ChildComponent,
+    HomeComponent,
+    AboutComponent,
+    PageNotFoundComponent,
+    PostsComponent,
+    PostComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
